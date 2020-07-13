7e044d55f99dad19bac9ff52a706bec311b67ccd
llewelyn@arcwave.co.uk <llewelyn@arcwave.co.uk>
2020-07-13 07:43:50 +0100

    Fixed .sh

--- robot/runrobot.sh
+++ robot/runrobot.sh
@@ -2,5 +2,5 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "Myservice service started at ${DATE}" | systemd-cat -p info
 
 python3 /home/pi/web-controlled-robot-full/robot/mqtt_picon_robot.py &
-python3 /home/pi/web-controlled-robot-full/web-server/streamvideo.py
+python3 /home/pi/web-controlled-robot-full/robot/streamvideo.py
 
